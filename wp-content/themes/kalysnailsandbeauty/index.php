52c5853ef8ccfc204aeaf9d55ec3755247526341
webbyrom <contact@web-byrom.com>
2022-10-26 11:09:55 +0200

    ajout de la police alexbrush

--- wp-content/themes/kalysnailsandbeauty/index.php
+++ wp-content/themes/kalysnailsandbeauty/index.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
 <div class="container container-index">
     <h1>Bienvenue sur le site: <?php wp_title(); ?></h1>
-<?php get_footer(); ?>
+    <h2><?php bloginfo('description'); ?></h2>
+    <?php get_footer(); ?>
