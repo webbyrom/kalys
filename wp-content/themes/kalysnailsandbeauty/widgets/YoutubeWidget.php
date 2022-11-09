0e1c407f88ea2c8ae24019ba630bc9aab07183d7
webbyrom <contact@web-byrom.com>
2022-11-09 22:11:10 +0100

    Widget youtube terminé

--- wp-content/themes/kalysnailsandbeauty/widgets/YoutubeWidget.php
+++ wp-content/themes/kalysnailsandbeauty/widgets/YoutubeWidget.php
@@ -5,6 +5,7 @@ class YoutubeWidget extends WP_Widget
     {
         parent::__construct('youtube_widget', 'Youtube Widget');
     }
+
     public function widget($args, $instance)
     {
         echo $args['before_widget'];
@@ -12,15 +13,31 @@ class YoutubeWidget extends WP_Widget
             $title = apply_filters('widget_title', $instance['title']);
             echo $args['before_title'] . $title . $args['after_title'];
         }
+        $youtube = isset($instance['youtube']) ? $instance['youtube'] : '';
+        echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . esc_attr($youtube) . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         echo $args['after_widget'];
     }
+
     public function form($instance)
     {
         $title = isset($instance['title']) ? $instance['title'] : '';
+        $youtube = isset($instance['youtube']) ? $instance['youtube'] : '';
 ?>
         <p>
             <label for="<?= $this->get_field_id('title') ?>">Titre</label>
-            <input class="widefat" type="text" name="<?= $this->get_field_name('title') ?>" value="<?= esc_attr($title) ?>" id="<? $this->get_field_name('title') ?>">
+            <input
+                class="widefat"
+                type="text" name="<?= $this->get_field_name('title') ?>"
+                value="<?= esc_attr($title) ?>"
+                id="<? $this->get_field_name('title') ?>">
+        </p>
+        <p>
+            <label for="<?= $this->get_field_id('youtube') ?>">Id Youtube</label>
+            <input
+                class="widefat"
+                type="text" name="<?= $this->get_field_name('youtube') ?>"
+                value="<?= esc_attr($youtube) ?>"
+                id="<? $this->get_field_name('youtube') ?>">
         </p>
 <?php
     }
