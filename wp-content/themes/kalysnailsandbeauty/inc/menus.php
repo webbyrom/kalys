eaaff2ae4b0a187043a9cf023226ba8fb66f5178
webbyrom <contact@web-byrom.com>
2022-11-14 13:17:23 +0100

    mise en place de l'architecture du footer via le widget( front n'est pas encore fait )

--- wp-content/themes/kalysnailsandbeauty/inc/menus.php
+++ wp-content/themes/kalysnailsandbeauty/inc/menus.php
@@ -1,4 +1,24 @@
 <?php
+
+/***
+ * déclaration du et autorisation du menu de navigation principal (header)
+ */
 add_action('after_setup_theme', function (){
     register_nav_menu('header', __('Menu principal', 'kalys'));
+});
+
+
+/***
+ * création de la navigation au niveau du footer via un widget
+ *
+ */
+add_action('widgets_init', function (){
+    register_sidebar([
+        'id'    => 'footer-nav',
+        'name'  => __('Footer_nav', 'kalys'),
+        'before_title' => '<div class="footer-title">',
+        'after_title' => '</div>',
+        'before_widget' => '<div class="footer_col">',
+        'afer_widget'   => '</div'
+    ]);
 });
