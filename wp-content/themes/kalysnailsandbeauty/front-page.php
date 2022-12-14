c278f979936c152a1e182f3e39957a51cdae72c4
Romain <54334592+webbyrom@users.noreply.github.com>
2022-12-14 12:07:01 +0100

    début styling css de la page accueil(h1, h2 + content)

--- wp-content/themes/kalysnailsandbeauty/front-page.php
+++ wp-content/themes/kalysnailsandbeauty/front-page.php
@@ -2,14 +2,23 @@
 
 
 <main class="kalys-main-accueil" id="kalys_main_accueil">
-    <?php
-    if (is_front_page()) {
-        echo do_shortcode('[rev_slider alias="slider-2"][/rev_slider]');
-    }
-    ?>
-    <H1 class="accueil-title"><?= get_bloginfo('name') . get_bloginfo('description') ?></H1>
-    <h2><?php the_title() ?></h2>
-    <?php the_content() ?>
+    <div class="kalys-header-slider">
+        <?php
+        if (is_front_page()) {
+            echo do_shortcode('[rev_slider alias="slider-2"][/rev_slider]');
+        }
+        ?>
+    </div>
+    <div class="kalys-col-full">
+        <H1 class="kalys-site-title"><?= get_bloginfo('name') . get_bloginfo('description') ?></H1>
+        <h2 class="kalys-title-page"><?php the_title() ?></h2>
+
+        <div class="kalys-accueil-section">
+            <?php the_content() ?>
+        </div>
+    </div>
+
+
 </main>
 
 
